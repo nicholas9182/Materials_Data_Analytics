7626f85a0abde97976b7325d0e30d08d636c3559
Nicholas Siemons <nicholassiemons@gmail.com>
2022-11-24 10:33:20 +0000

    cli_improvements

--- cli_tools/plot_hills.py
+++ cli_tools/plot_hills.py
@@ -1,16 +1,18 @@
+#!/usr/bin/env python3
 import click
 import plumed as pl
 from analytics.metadynamics.free_energy import FreeEnergyLandscape
 
 
 @click.command()
-@click.option("--file", "-f", default="HILLS", help="Hills file to plot")
-@click.option("--output", "-o", default=".", help="Output directory for figures")
-def main(file, output):
+@click.option("--file", "-f", default="HILLS", help="Hills file to plot", type=str)
+@click.option("--output", "-o", default=".", help="Output directory for figures", type=str)
+@click.option("--time_resolution", "-tr", default=None, help="Number of decimal places for time values", type=int)
+def main(file: str, output: str, time_resolution: int):
 
     hills = pl.read_as_pandas(file)
     landscape = FreeEnergyLandscape(hills)
-    figures = landscape.get_hills_figures()
+    figures = landscape.get_hills_figures(time_resolution=time_resolution)
 
     for key, value in figures.items():
         save_dir = output + "/Walker_" + str(key) + ".pdf"
