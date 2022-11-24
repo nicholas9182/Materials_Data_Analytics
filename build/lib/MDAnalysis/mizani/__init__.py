074a25e1b027598cb12decd505fc712f94df4e8c
Nicholas Siemons <34894723+nicholas9182@users.noreply.github.com>
2022-11-24 11:10:26 +0000

    Delete build directory

--- build/lib/MDAnalysis/mizani/__init__.py
+++
@@ -1,8 +0,0 @@
-from importlib.metadata import version, PackageNotFoundError
-
-
-try:
-    __version__ = version('mizani')
-except PackageNotFoundError:
-    # package is not installed
-    pass
--- build/lib/MDAnalysis/plotnine/mapping/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Aesthetic Mappings
-"""
-from .aes import aes
-from .evaluation import after_stat, after_scale, stage
-
-
-__all__ = ['aes', 'after_stat', 'after_scale', 'stage']
