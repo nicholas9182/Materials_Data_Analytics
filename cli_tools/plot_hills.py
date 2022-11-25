ffff908f1840789e9fe4572f7a4e354f2888f1a9
Nicholas Siemons <nicholassiemons@gmail.com>
2022-11-25 21:54:46 +0000

    max_and_mean_added_to_plots

--- cli_tools/plot_hills.py
+++ cli_tools/plot_hills.py
@@ -12,7 +12,7 @@ from analytics.metadynamics.free_energy import FreeEnergyLandscape
 @click.option("--height_power", "-hp", default=1, help="Power to raise height of hills for easier visualisation", type=float)
 def main(file: str, output: str, time_resolution: int, height_power: float):
     """
-    cli tool to plot hill heights for all walkers, as well as the value of their CV
+    cli tool to plot hill heights for all walkers, as well as the value of their CV. It also plots the average and max hills deposited
     :param file: the location of the HILLS file
     :param output: folder in which to put images
     :param time_resolution: how to bin the t axis for faster plotting
@@ -29,6 +29,15 @@ def main(file: str, output: str, time_resolution: int, height_power: float):
         value.write_image(save_dir, scale=2)
         current_time = datetime.now().strftime("%H:%M:%S")
         click.echo(f"{current_time}: Made Walker_{key}.pdf in {output}", err=True)
+
+    average_hills = landscape.get_average_hills_figure(time_resolution=time_resolution)
+    average_hills.write_image(output + "/hills_mean.pdf", scale=2)
+    current_time = datetime.now().strftime("%H:%M:%S")
+    click.echo(f"{current_time}: Made hills_mean.pdf in {output}", err=True)
+    max_hills = landscape.get_max_hills_figure(time_resolution=time_resolution)
+    max_hills.write_image(output + "/hills_max.pdf", scale=2)
+    current_time = datetime.now().strftime("%H:%M:%S")
+    click.echo(f"{current_time}: Made hills_max.pdf in {output}", err=True)
 
 
 if __name__ == "__main__":
--- visualisation/themes.py
+++ visualisation/themes.py
@@ -4,12 +4,12 @@ custom_dark_template = dict(
     layout=go.Layout(
         plot_bgcolor='black',
         paper_bgcolor='black',
-        xaxis={'color': '#625D5D', 'dividercolor': '#625D5D', 'gridcolor': 'black', 'linecolor': 'black', 'showline': False, 'showgrid': False},
-        yaxis={'color': '#625D5D', 'dividercolor': '#625D5D', 'gridcolor': 'black', 'linecolor': 'black', 'showline': False, 'showgrid': False},
-        margin={'t': 40, 'b': 40, 'l': 40, 'r': 40},
+        xaxis={'color': '#85929E', 'dividercolor': '#625D5D', 'gridcolor': 'black', 'linecolor': 'black', 'showline': False, 'showgrid': False},
+        yaxis={'color': '#85929E', 'dividercolor': '#625D5D', 'gridcolor': 'black', 'linecolor': 'black', 'showline': False, 'showgrid': False},
+        margin={'t': 60, 'b': 60, 'l': 60, 'r': 60},
         width=1000,
         height=600,
         colorway=['#E5E4E2'],
-        font={'color': '#E5E4E2'}
+        font={'color': '#EAECEE'}
     )
 )
