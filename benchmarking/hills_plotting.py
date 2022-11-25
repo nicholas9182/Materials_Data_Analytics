ce5e6ba9dd0eea47ff1eb5fda251313e7129a5d2
Nicholas Siemons <nicholassiemons@gmail.com>
2022-11-25 10:04:58 +0000

    fixed_plotter_mean

--- benchmarking/hills_plotting.py
+++ benchmarking/hills_plotting.py
@@ -1,13 +1,12 @@
 import plumed as pl
 import pandas as pd
-import numpy as np
 from timer import Timer
 from analytics.metadynamics.free_energy import FreeEnergyLandscape
 
 
-def benchmark_plotting_hills_v1() -> pd.DataFrame:
+def benchmark_plotting_hills_v1(time_res: int) -> pd.DataFrame:
     """
-    old way of formatting the hills that is very slow
+    old way of formatting the hills that is very slow using apply and assign
     :return:
     """
     data = pl.read_as_pandas("/Users/nicholassiemons/Dropbox/OBT/0091/002/HILLS")
@@ -16,7 +15,7 @@ def benchmark_plotting_hills_v1() -> pd.DataFrame:
     cvs = landscape.cvs
     long_hills = (hills
                   .melt(value_vars=cvs+['height'], id_vars=['time', 'walker'])
-                  .assign(time=lambda x: x['time'].round(2))
+                  .assign(time=lambda x: x['time'].round(time_res))
                   .groupby(['time', 'walker', 'variable'], group_keys=False)
                   .apply(lambda x: x.assign(value=lambda y: y['value'].mean()))
                   .drop_duplicates()
@@ -25,9 +24,9 @@ def benchmark_plotting_hills_v1() -> pd.DataFrame:
     return long_hills
 
 
-def benchmark_plotting_hills_v2() -> pd.DataFrame:
+def benchmark_plotting_hills_v2(time_res: int) -> pd.DataFrame:
     """
-    old way of formatting the hills that is very slow
+    option where we just take a random row in each group by dropping duplicates across walker, time and variable. Not ideal but seems much faster
     :return:
     """
     data = pl.read_as_pandas("/Users/nicholassiemons/Dropbox/OBT/0091/002/HILLS")
@@ -36,8 +35,28 @@ def benchmark_plotting_hills_v2() -> pd.DataFrame:
     cvs = landscape.cvs
     long_hills = (hills
                   .melt(value_vars=cvs+['height'], id_vars=['time', 'walker'])
-                  .assign(time=lambda x: x['time'].round(2))
+                  .assign(time=lambda x: x['time'].round(time_res))
                   .drop_duplicates(subset=['time', 'walker', 'variable'])
+                  )
+
+    return long_hills
+
+
+def benchmark_plotting_hills_v3(time_res: int) -> pd.DataFrame:
+    """
+    try using the groupby and .mean()
+    :return:
+    """
+    data = pl.read_as_pandas("/Users/nicholassiemons/Dropbox/OBT/0091/002/HILLS")
+    landscape = FreeEnergyLandscape(data)
+    hills = landscape.hills
+    cvs = landscape.cvs
+    long_hills = (hills
+                  .melt(value_vars=cvs+['height'], id_vars=['time', 'walker'])
+                  .assign(time=lambda x: x['time'].round(time_res))
+                  .groupby(['time', 'walker', 'variable'], group_keys=False)
+                  .mean()
+                  .reset_index()
                   )
 
     return long_hills
@@ -48,11 +67,13 @@ if __name__ == "__main__":
     t = Timer()
 
     t.start()
-    r1 = benchmark_plotting_hills_v1()
+    r1 = benchmark_plotting_hills_v1(1)
     t.stop()
 
     t.start()
-    r2 = benchmark_plotting_hills_v2()
+    r2 = benchmark_plotting_hills_v2(1)
     t.stop()
 
-    print(r1.equals(r2))
+    t.start()
+    r3 = benchmark_plotting_hills_v3(1)
+    t.stop()
