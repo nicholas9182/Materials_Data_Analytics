a18782b08ce16cfcecb408082897588d4bba2474
Nicholas Siemons <nicholas9182@gmail.com>
2024-01-30 17:33:45 -0800

    added_get_bonds

--- build/lib/analytics/quantum_chemistry/gaussian.py
+++ build/lib/analytics/quantum_chemistry/gaussian.py
@@ -15,7 +15,6 @@ class GaussianParser:
         self._log_file = log_file
         self._lines = [line for line in open(log_file, 'r')]
         self._keywords = [k for k in self._lines if '#p ' in k][0].split()[1:]
-        self._multiplicity = int([m for m in self._lines if 'Charge =' in m][0][27:])
         self._raman = True if len([r for r in self._keywords if 'raman' in r]) > 0 else False
         self._opt = True if len([r for r in self._keywords if 'opt' in r]) > 0 else False
         self._complete = True if len([r for r in self._lines if 'Normal termination of ' in r]) > 0 else False
@@ -25,8 +24,10 @@ class GaussianParser:
 
         if len([c for c in self._lines if 'Charge =' in c]) > 0:
             self._charge = int([c for c in self._lines if 'Charge =' in c][0][9:].split()[0])
+            self._multiplicity = int([m for m in self._lines if 'Charge =' in m][0][27:])
         else:
             self._charge = None
+            self._multiplicity = None
 
         if len([e for e in self._lines if 'SCF Done' in e]) > 0:
             self._energy = float([e for e in self._lines if 'SCF Done' in e][-1].split()[4]) * 2625.5
