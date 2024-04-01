19f43f39b0e67f7bec5dee65acd99f9e39cce36d
Nicholas Siemons <nicholas9182@gmail.com>
2024-04-01 11:13:17 +0100

    mep

--- build/lib/test/test_path.py
+++ build/lib/test/test_path.py
@@ -2,8 +2,6 @@ import unittest
 import numpy as np
 import pandas as pd
 from analytics.metadynamics.path_analysis import Path
-from analytics.metadynamics.free_energy import FreeEnergySpace
-from analytics.metadynamics.path_analysis import SurfacePath
 
 
 class TestPath(unittest.TestCase):
@@ -27,19 +25,3 @@ class TestPath(unittest.TestCase):
         self.assertTrue(path['CM6'].iloc[4] == 4.8)
         self.assertTrue(path['CM7'].iloc[4] == 0.0)
 
-    def test_get_surface_force(self):
-
-        points = [[3, 0], [0, 0], [0, 3]]
-        cvs = ['CM2', 'CM3']
-
-        surface = (FreeEnergySpace
-                   .from_standard_directory("../test_trajectories/ndi_na_binding/")
-                   .get_reweighted_surface(cvs=["CM2", "CM3"], bins=[-0.5, 0.5, 1.5, 2.5, 3.5])
-                   .set_as_symmetric('y=x')
-                   )
-
-        path = SurfacePath.from_points(points, n_steps=21, cvs=cvs, shape=surface)
-
-        forces = path._get_surface_forces(index=5)
-
-        self.assertTrue(type(forces) == pd.DataFrame)
